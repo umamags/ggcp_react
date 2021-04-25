f565d5ec1087b34e29ab28974c8825b489c4fcca
Natarajan <umamags@gmail.com>
2021-04-25 11:11:41 +0530

    changed text

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to Giants Group of Chennai Pushkar/i);
+  const linkElement = screen.getByText(/South Chennai chapter of the International Giants Organization/i);
   expect(linkElement).toBeInTheDocument();
 });
